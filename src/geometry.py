01703df1b8253ae0cde6f871871c409767472b1f
Homomorpheus <e12225948@student.tuwien.ac.at>
2025-05-09 15:09:36 +0200

    refactor: now using newmark

--- src/geometry.py
+++ src/geometry.py
@@ -348,17 +348,17 @@ class Ball:
             qold = qold[0:2]
             vold = vold[0:2]
 
-            old, step_out = solvers.generalized_alpha(qold, vold, self.mass_mat_ode, np.zeros((2,2)),
-                                                      lambda q, v: self._mass*self._gravity(q, v), steps-step,
-                                                      t_end * (steps - step)/steps, callback_ode
-                                                     )
+            old, step_out = solvers.newmark(qold, vold, self.mass_mat_ode, np.zeros((2,2)),
+                                            lambda q, v: self._mass*self._gravity(q, v), steps-step,
+                                            t_end * (steps - step)/steps, callback_ode
+                                            )
             qold, vold = self.ode_data_to_dae_data(old)
             step += step_out
         
         while step < steps:
-            old, step_out = solvers.generalized_alpha(qold, vold, self.mass_mat_dae, self.damping_mat, self.force, steps - step,
-                                                      t_end * (steps - step)/steps, callback_dae
-                                                     )
+            old, step_out = solvers.newmark(qold, vold, self.mass_mat_dae, self.damping_mat, self.force, steps - step,
+                                            t_end * (steps - step)/steps, callback_dae
+                                            )
             qold = old[0:2]
             vold = old[3:5]
             step += step_out
@@ -366,10 +366,10 @@ class Ball:
             if step >= steps:
                 break
 
-            old, step_out = solvers.generalized_alpha(qold, vold, self.mass_mat_ode, np.zeros((2,2)),
-                                                      lambda q, v: self._mass*self._gravity(q, v), steps-step,
-                                                      t_end * (steps - step)/steps, callback_ode
-                                                     )
+            old, step_out = solvers.newmark(qold, vold, self.mass_mat_ode, np.zeros((2,2)),
+                                            lambda q, v: self._mass*self._gravity(q, v), steps-step,
+                                            t_end * (steps - step)/steps, callback_ode
+                                            )
             qold, vold = self.ode_data_to_dae_data(old)
             step += step_out
             
--- src/solvers.py
+++ src/solvers.py
@@ -26,7 +26,7 @@ def generalized_alpha(q0: np.ndarray, v0: np.ndarray, M: np.ndarray, C: np.ndarr
         callback -- function of step-index and current (q,v,a), gets called every step
         rhoinfty -- stability parameter, see links above
     Output:
-        last (q,v,a) calculated
+        last (q,v,a), step-number calculated
     """
     
     h = t_end/steps
@@ -79,33 +79,63 @@ def generalized_alpha(q0: np.ndarray, v0: np.ndarray, M: np.ndarray, C: np.ndarr
             
     return old, step
 
+
+def newmark(q0: np.ndarray, v0: np.ndarray, M: np.ndarray, C: np.ndarray, force, steps, t_end, callback=None):
+    """
+    Newmark method.
+    For documentation on the algorithm, see:
+        https://jschoeberl.github.io/IntroSC/ODEs/mechanical.html#the-newmark-method
+        https://miaodi.github.io/finite%20element%20method/newmark-generalized/
+    Input:
+        q0 -- starting position
+        v0 -- starting velocity
+        M  -- mass matrix
+        C  -- damping matrix
+        force -- force, a function of q (position)
+        steps -- amount of ODE/DAE solver iterations
+        t_end -- end time
+        callback -- function of step-index and current (q,v,a), gets called every step
+    Output:
+        last (q,v,a), step-number calculated
+    """
     
-# def newmark_beta(q0: np.ndarray, v0: np.ndarray, M: np.ndarray, force, steps, t_end, callback=None, beta=1, gamma=1):
-#     Minv = np.linalg.inv(M)
-#     h = t_end/steps
-#     a0 = Minv @ force(q0, v0)
-# 
-#     old = np.concatenate([q0, v0, a0])
-# 
-#     # implicit solver equations
-#     def eqs(new):
-#         qnew = new[:len(q0)]
-#         vnew = new[len(q0):len(q0)+len(v0)]
-#         anew = new[len(q0)+len(v0):]
-# 
-#         qold = old[:len(q0)]
-#         vold = old[len(q0):len(q0)+len(v0)]
-#         aold = old[len(q0)+len(v0):]
-# 
-#         # https://miaodi.github.io/finite%20element%20method/newmark-generalized/
-#         eq_eval = np.concatenate([(M@anew.transpose()).transpose() - force(qnew, vnew),
-#                                  qold - qnew + h*vold + h**2/2*((1-2*beta)*aold + 2*beta*anew),
-#                                  vold - vnew + h*((1-gamma)*aold + gamma*anew)])
-#         return eq_eval
-#         
-#     for step in range(steps):
-#         new = fsolve(eqs, old)
-#         old=new
-#         if callback != None:
-#             callback(step, new)
-#     return new
+    h = t_end/steps
+    dim = len(q0)
+    #starting value for a
+    a0 = np.zeros(dim)
+
+    # state from before the timestep
+    old = np.concatenate([q0, v0, a0])
+
+    # implicit solver equations
+    # new is qnew, vnew, anew; each of length dim
+    def eqs(new: np.array):
+        qnew, vnew, anew = np.split(new, len(new)/dim)
+
+        eq_eval = np.concatenate([(M@anew.transpose()).transpose() + (C@vnew.transpose()).transpose() - force(qnew, vnew),
+                                  vnew - vold - h/2*(anew + aold),
+                                  qnew - qold - h*vold - h**2/4*(anew + aold)
+                                  ])
+        return eq_eval
+        
+    for step in range(1,steps+1):
+        # solve the solver's equations
+        qold, vold, aold = np.split(old, len(old)/dim)
+        sol = root(eqs, old)
+
+        if not sol.success:
+            if sol.status == 5:
+                warnings.warn(sol.message, RuntimeWarning)
+            else:
+                callback(step, old, error=True)
+                raise RuntimeError(F"fscipy.optimize.root did not converge (message: {sol.message})")
+        
+        old=sol.x
+        
+        if callback != None:
+            terminate = callback(step, old, error=False)
+
+        if terminate == True:
+            break
+            
+    return old, step
